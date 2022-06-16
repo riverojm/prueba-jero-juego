d68b60ae70d113bdb4ab18a57b4b36a040ed6a07
juanma rivero <45921725+riverojm@users.noreply.github.com>
2022-06-15 21:31:31 -0300

    COMMIT
    TERMINADISIMO

---
+++ Assets/RandomGanar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGanar : MonoBehaviour
+{       
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += new Vector3(Random.Range(25, 5), Random.Range(2, 30), Random.Range(20, -20));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }    
+}
--- Assets/Volver/VolverBotones.cs
+++ Assets/Volver/VolverBotones.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class VolverJugar : MonoBehaviour
+public class VolverBotones : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,13 +14,16 @@ public class VolverJugar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.R))
-        {
-            SceneManager.LoadScene("Juego");
-        }
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+        
+    }
+
+    public void VolverJugar()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
     }
 }
